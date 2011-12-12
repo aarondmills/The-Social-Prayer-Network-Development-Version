3338072b052516cc9e025a24ea54b55ef84d5f56
Aaron Mills <aaron@jailbreakcreative.com>
2011-12-12 00:18:53 -0500

    added pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def contact
+  end
+
+  def terms
+  end
+
+  def privacy
+  end
+
+  def credits
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Spn::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/contact"
+
+  get "pages/terms"
+
+  get "pages/privacy"
+
+  get "pages/credits"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
