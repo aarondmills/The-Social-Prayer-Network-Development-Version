891e2dbf87335cce8afdd6b954634771bcceaab6
Aaron Mills <aaron@jailbreakcreative.com>
2011-12-11 22:48:20 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spn::Application.config.secret_token = '9b61f97bcfad4ce3f744a7194b424f70bd0809d687d92595cb571b7e6e6a843de3f540625f9955e0981129cf036f81b7c1b18d1f84c820f9581da8006975da14'
