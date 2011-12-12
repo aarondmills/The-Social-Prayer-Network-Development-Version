c366b2cc0e82db4cfb96ae02e5a14c5163d49cbe
Aaron Mills <aaron@jailbreakcreative.com>
2011-12-12 15:18:51 -0500

    fixed twitter

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, '	eGHmrjmD6b57Wy13ui6I6A', 'KWnC5EX0DAbCPjpBMTNT1YQxWWBM6P6sz9a1BkoVM'
+  provider :twitter, 'eGHmrjmD6b57Wy13ui6I6A', 'KWnC5EX0DAbCPjpBMTNT1YQxWWBM6P6sz9a1BkoVM'
 end
+
 
 
