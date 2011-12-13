d9279b88cfa6533511e2fe0a50faa073615865ad
Aaron Mills <aaron@jailbreakcreative.com>
2011-12-13 09:38:03 -0500

    fixed facebook and twitter

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['TWITTERID'], ENV['TWITTERKEY']
-  provider :facebook, ENV['FACEBOOKID'], ENV['FACEBOOKKEY']
+  provider :twitter, TWITTERID,TWITTERKEY
+  provider :facebook, FACEBOOKID,FACEBOOKKEY
 	provider :identity, on_failed_registration: lambda { |env|    
     IdentitiesController.action(:new).call(env)
   }
